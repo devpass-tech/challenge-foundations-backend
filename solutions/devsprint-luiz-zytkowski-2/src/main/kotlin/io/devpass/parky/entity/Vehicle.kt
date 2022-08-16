b0221689579740b1ec5b5a5580a7e353cdd7d9f5
Mariana Cavalcante <marianacavalcante.nasc@gmail.com>
2022-08-16 14:57:34 -0300

    create entity for vehicle

---
+++ solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/entity/Vehicle.kt
@@ -0,0 +1,20 @@
+package io.devpass.parky.entity
+
+import org.hibernate.annotations.CreationTimestamp
+import java.time.LocalDateTime
+import javax.persistence.Entity
+import javax.persistence.Id
+import kotlin.math.absoluteValue
+import kotlin.random.Random
+
+@Entity
+data class Vehicle(
+    @Id
+    var vehicleId: Int = Random.nextInt().absoluteValue,
+    var brand: String,
+    var vehicleColor: String,
+    var owner: String,
+    @CreationTimestamp
+    var createdAt: LocalDateTime = LocalDateTime.now()
+)
+
