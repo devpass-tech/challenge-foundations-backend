2ccece993e7a3a07088a639ada5a8b3fa5d255eb
livioqueiroz <queiroz.livio@gmail.com>
2022-08-17 14:40:32 -0300

    Alterado nome da interface

--- solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/repository/ParkingRepository.kt
+++ solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/repository/ParkingRepository.kt
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ParkingRepository: CrudRepository<ParkingSpot, Int> {
+interface ParkingSpotRepository: CrudRepository<ParkingSpot, Int> {
 }
