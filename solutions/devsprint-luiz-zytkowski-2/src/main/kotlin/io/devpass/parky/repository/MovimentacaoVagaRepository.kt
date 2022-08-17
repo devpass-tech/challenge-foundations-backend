35d4a0fa01f67657257b6e093000e3875d7a5264
Cintia Requião <cintia.rl94@gmail.com>
2022-08-17 17:29:03 -0300

    Revert "Cria Repository de MovimentacaoVaga"
    This reverts commit 8e4677ec596a9c44adaacc5fae10d2b370c33d66.

--- solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/repository/MovimentacaoVagaRepository.kt
+++
@@ -1,9 +0,0 @@
-package io.devpass.parky.repository
-
-import io.devpass.parky.entity.MovimentacaoVaga
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface MovimentacaoVagaRepository : CrudRepository<MovimentacaoVaga, Int> {
-}
