288b9f5a9c031cf55e1a5618afc9f3d34defffc6
Rielly <rielly@cora.com.br>
2022-08-17 17:34:06 -0300

    [KT2-7] Add changes requested

--- solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/entity/ParkingSpotMovement.kt
+++ solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/entity/ParkingSpotMovement.kt
@@ -4,19 +4,14 @@ import org.hibernate.annotations.CreationTimestamp
 import java.time.LocalDateTime
 import javax.persistence.Entity
 import javax.persistence.Id
-import kotlin.math.absoluteValue
-import kotlin.random.Random
 
 @Entity
 data class ParkingSpotMovement(
         @Id
-        var id: Int = Random.nextInt().absoluteValue,
         var parking_spot_id: Int,
-        var floor: Int,
-        var spot: Int,
-        var inUseBy: Int,
+        var event: Int,
         @CreationTimestamp
-        var createdAt: LocalDateTime = LocalDateTime.now()
+        var createdAt: LocalDateTime
 )
 
 
