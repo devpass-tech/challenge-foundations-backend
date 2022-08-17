369b7f4ee76264322f6d376a0bf88b991021e51f
Cintia Requião <cintia.rl94@gmail.com>
2022-08-17 15:35:46 -0300

    Cria Controller de Movimentação de Vagas

---
+++ solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/controller/ParkingSpotMovementController.kt
@@ -0,0 +1,11 @@
+package io.devpass.parky.controller
+
+import io.devpass.parky.service.ParkingSpotMovementService
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/parking-spots")
+class ParkingSpotMovementController(
+    private val parkingSpotMovementRepository: ParkingSpotMovementRepository
+    ) {}
