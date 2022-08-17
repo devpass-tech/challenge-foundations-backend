1ee278de1f2e024bf1e0084b13712b26ee9830fa
Cintia Requião <cintia.rl94@gmail.com>
2022-08-17 17:29:09 -0300

    Revert "Alterado nome da interface"
    This reverts commit 81ccf7fdfe8f1cb085e286cc4044dbe16793db22.

--- solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/repository/ParkingRepository.kt
+++ solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/repository/ParkingRepository.kt
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ParkingSpotRepository: CrudRepository<ParkingSpot, Int> {
+interface ParkingRepository: CrudRepository<ParkingSpot, Int> {
 }
