b8ba349d5fea4a54f156f7bff001e614ead40863
pedroajs <pedroajs93@gmail.com>
2022-08-18 11:52:55 -0300

    criando método de encontrar um veículo pelo seu ID

--- solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/service/VehicleService.kt
+++ solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/service/VehicleService.kt
@@ -1,5 +1,6 @@
 package io.devpass.parky.service
 
+import io.devpass.parky.entity.Vehicle
 import io.devpass.parky.repository.VehicleRepository
 import org.springframework.stereotype.Service
 
@@ -7,4 +8,9 @@ import org.springframework.stereotype.Service
 class VehicleService(
     private val vehicleRepository: VehicleRepository,
 ) {
+    fun findById(vehicleId: Int): Vehicle?{
+        return vehicleRepository.findById(vehicleId).let {
+            if(it.isPresent) it.get() else null
+        }
+    }
 }
