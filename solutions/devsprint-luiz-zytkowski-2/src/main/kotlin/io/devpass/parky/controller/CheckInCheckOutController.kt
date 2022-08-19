f46205260e6624103d5d41d5d3db56c03b709d0f
Rielly <rielly@cora.com.br>
2022-08-19 15:43:30 -0300

    [KT2-23] Add endpoint of check-in

--- solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/controller/CheckInCheckOutController.kt
+++ solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/controller/CheckInCheckOutController.kt
@@ -1,12 +1,23 @@
 package io.devpass.parky.controller
 
+import io.devpass.parky.requests.CheckInRequest
 import io.devpass.parky.service.CheckInOutService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/check-in-out")
 class CheckInCheckOutController(
-    private val checkInOutService: CheckInOutService,
+        private val checkInOutService: CheckInOutService,
 ) {
+    @PostMapping
+    fun createCheckIn(
+            @RequestBody checkInRequest: CheckInRequest
+    ): ResponseEntity<String> {
+        checkInOutService.createCheckIn(checkInRequest)
+        return ResponseEntity.ok("Deu bom")
+    }
 }
