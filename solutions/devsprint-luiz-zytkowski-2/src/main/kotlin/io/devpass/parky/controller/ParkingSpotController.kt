9f65b08bf5cc50db91ca32816ab1cb995ab3e487
pedroajs <pedroajs93@gmail.com>
2022-08-19 19:11:51 -0300

    endpoint que retorna um parkingspot

--- solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/controller/ParkingSpotController.kt
+++ solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/controller/ParkingSpotController.kt
@@ -1,6 +1,9 @@
 package io.devpass.parky.controller
 
+import io.devpass.parky.entity.ParkingSpot
+import io.devpass.parky.repository.ParkingSpotRepository
 import io.devpass.parky.service.ParkingSpotService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,4 +11,9 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/parking-spot")
 class ParkingSpotController(
     private val parkingSpotService: ParkingSpotService,
-)
+){
+    @GetMapping("/parking-spot-random-vacancy")
+    fun getRandomEmptyParkingSpot(): ParkingSpot {
+        return parkingSpotService.findEmptyParkingSpotAtRandom()
+    }
+}
