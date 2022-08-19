b9a0c815e37926ab3bb6ba572e7b71c18b441ade
pedroajs <pedroajs93@gmail.com>
2022-08-19 19:12:35 -0300

    WIP: query no repositorio para trazer uma parkingspot vazia.

--- solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/repository/ParkingSpotRepository.kt
+++ solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/repository/ParkingSpotRepository.kt
@@ -1,8 +1,14 @@
 package io.devpass.parky.repository
 
 import io.devpass.parky.entity.ParkingSpot
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ParkingSpotRepository : CrudRepository<ParkingSpot, Int>
+interface ParkingSpotRepository : CrudRepository<ParkingSpot, Int>{
+
+    //ver a melhor ou a forma correta de fazer a query
+    @Query("SELECT  from ParkingSpot where ParkingSpot.inUseBy = inUseBy")
+    fun getRandomEmptyParkingSpot(): List<ParkingSpot>
+}
