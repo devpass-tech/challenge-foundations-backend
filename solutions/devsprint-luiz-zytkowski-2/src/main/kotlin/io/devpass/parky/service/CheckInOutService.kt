90bd751700a1e1251f9636432b4a9936ac9578e6
elaine mattos <elaine.mattos@outlook.pt>
2022-08-22 15:17:20 -0300

    método de check-out de Check(in/out)
    Co-authored-by: Ci <cintia.rl94@gmail.com>

--- solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/service/CheckInOutService.kt
+++ solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/service/CheckInOutService.kt
@@ -1,5 +1,8 @@
 package io.devpass.parky.service
 
+import io.devpass.parky.entity.ParkingSpotMovement
+import io.devpass.parky.entity.Vehicle
+import io.devpass.parky.requests.CheckInRequest
 import org.springframework.stereotype.Service
 
 @Service
@@ -8,4 +11,49 @@ class CheckInOutService(
     private val vehicleService: VehicleService,
     private val parkingSpotService: ParkingSpotService,
 ) {
+
+    fun createCheckIn(checkIn: CheckInRequest) {
+
+        val vehicle = vehicleService.create(
+            Vehicle(
+                brand = checkIn.vehicle.brand,
+                color = checkIn.vehicle.color,
+                owner = checkIn.vehicle.owner
+            )
+        )
+
+        val parkingSpot = parkingSpotService.findById(checkIn.parkingSpotId)
+            ?: throw Exception("Vaga não encontrada")
+
+        if (parkingSpot.inUseBy != null) {
+            throw Exception("Vaga ocupada pelo veículo: $vehicle")
+        }
+
+        parkingSpot.inUseBy = vehicle.id
+        parkingSpotService.update(parkingSpot)
+
+        val parkingSpotMovement = ParkingSpotMovement(
+            parkingSpotId = parkingSpot.id,
+            event = "Check-in realizado pelo veículo: ${vehicle.id}"
+        )
+        parkingSpotMovementService.create(parkingSpotMovement)
+    }
+
+    fun delete(id: Int) {
+        val parkingSpot = parkingSpotService.findById(id)
+            ?: throw Exception("Vaga não encontrada")
+
+        if (parkingSpot.inUseBy == null) {
+            throw Exception("Vaga livre")
+        }
+
+        val parkingSpotMovement = ParkingSpotMovement(
+            parkingSpotId = id,
+            event = "Check-out realizado pelo veículo: ${parkingSpot.inUseBy}"
+        )
+        parkingSpotMovementService.create(parkingSpotMovement)
+
+        parkingSpot.inUseBy = null
+        parkingSpotService.update(parkingSpot)
+    }
 }
