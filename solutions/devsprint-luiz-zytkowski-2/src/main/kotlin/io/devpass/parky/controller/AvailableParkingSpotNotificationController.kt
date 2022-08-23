0cd59b0c171df22021ea1d148aec9be42ccb720e
pedroajs <pedroajs93@gmail.com>
2022-08-23 16:16:30 -0300

    Criando rota para notificação de vaga.
    Co-authored-by: elaine <elaine.mattos@outlook.pt>

--- solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/controller/AvailableParkingSpotNotificationController.kt
+++ solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/controller/AvailableParkingSpotNotificationController.kt
@@ -1,6 +1,12 @@
 package io.devpass.parky.controller
 
+import io.devpass.parky.entity.ParkingSpot
+import io.devpass.parky.repository.AvailableParkingSpotNotificationRepository
+import io.devpass.parky.requests.AvailableParkingSpotNotificationRequest
 import io.devpass.parky.service.AvailableParkingSpotNotificationService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,4 +14,13 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/available-parking-spot")
 class AvailableParkingSpotNotificationController(
     private val availableParkingSpotNotificationService: AvailableParkingSpotNotificationService,
-)
+){
+    //criar endpoint para o usuario criar notificaçao
+    //post
+
+    @PostMapping
+    fun createNotification(
+        @RequestBody availableParkingSpotNotificationRequest: AvailableParkingSpotNotificationRequest
+    ) =
+        availableParkingSpotNotificationService.createNotification(availableParkingSpotNotificationRequest)
+}
--- solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/service/AvailableParkingSpotNotificationService.kt
+++ solutions/devsprint-luiz-zytkowski-2/src/main/kotlin/io/devpass/parky/service/AvailableParkingSpotNotificationService.kt
@@ -1,9 +1,20 @@
 package io.devpass.parky.service
 
+import io.devpass.parky.entity.AvailableParkingSpotNotification
 import io.devpass.parky.repository.AvailableParkingSpotNotificationRepository
+import io.devpass.parky.requests.AvailableParkingSpotNotificationRequest
 import org.springframework.stereotype.Service
 
 @Service
 class AvailableParkingSpotNotificationService(
     private val availableParkingSpotNotificationRepository: AvailableParkingSpotNotificationRepository,
-)
+){
+    fun createNotification(availableParkingSpotNotificationRequest: AvailableParkingSpotNotificationRequest){
+        availableParkingSpotNotificationRepository.save(
+            AvailableParkingSpotNotification(
+                parkingSpotId = availableParkingSpotNotificationRequest.parkingSpotId,
+                email = availableParkingSpotNotificationRequest.email,
+            )
+        )
+    }
+}
