46443f480b0e47bfdf92d3a89b5aa67cd0e1be14
Gustavo <gustavodacosta@gmail.com>
2023-02-07 22:59:01 -0300

    add ParkingSpoRepository

---
+++ solutions/devsprint-michel-ferreira-2/src/main/kotlin/io/devpass/parky/repository/ParkingSpotRepository.kt
@@ -0,0 +1,8 @@
+package io.devpass.parky.repository
+
+import io.devpass.parky.entity.ParkingSpot
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ParkingSpotRepository : CrudRepository<ParkingSpot, Int>
