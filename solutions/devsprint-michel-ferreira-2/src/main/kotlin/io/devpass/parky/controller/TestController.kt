b796cc9481c77c02ec966fc0339c8ebbb9d2cf80
michel <michel.ferreira.silva@gmail.com>
2023-02-08 19:04:22 -0300

    Endpoint de testes

---
+++ solutions/devsprint-michel-ferreira-2/src/main/kotlin/io/devpass/parky/controller/TestController.kt
@@ -0,0 +1,25 @@
+package io.devpass.parky.controller
+
+import io.devpass.parky.entity.Vehicle
+import io.devpass.parky.service.ParkingSpotEventService
+import io.devpass.parky.service.ParkingSpotService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/tests")
+class TestController(
+    private val parkingSpotEventService: ParkingSpotEventService,
+    private val parkingSpotService: ParkingSpotService,
+    private val vehSpotService: ParkingSpotService
+) {
+
+    @GetMapping("/all-vehicles")
+    fun getAllVehicles() = listOf<Vehicle>(
+        Vehicle(1, "Honda", "Branco", "Michel")
+    )
+
+
+
+}
--- solutions/devsprint-michel-ferreira-2/src/main/kotlin/io/devpass/parky/repository/VehicleRepository.kt
+++ solutions/devsprint-michel-ferreira-2/src/main/kotlin/io/devpass/parky/repository/VehicleRepository.kt
@@ -1,8 +1,8 @@
 package io.devpass.parky.repository
 
-import io.devpass.parky.entity.VacancyMovement
+import io.devpass.parky.entity.Vehicle
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface VehicleRepository: CrudRepository<VacancyMovement, Int>
+interface VehicleRepository : CrudRepository<Vehicle, Int>
