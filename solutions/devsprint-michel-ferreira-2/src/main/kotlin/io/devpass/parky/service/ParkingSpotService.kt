f60fa0e15ad27d02995f64ab348f0f651dc1e285
bino1 <r.asera@outlook.com>
2023-02-10 08:58:24 -0300

    Criando método de listagem de todas as vagas no service

--- solutions/devsprint-michel-ferreira-2/src/main/kotlin/io/devpass/parky/service/ParkingSpotService.kt
+++ solutions/devsprint-michel-ferreira-2/src/main/kotlin/io/devpass/parky/service/ParkingSpotService.kt
@@ -1,14 +1,20 @@
 package io.devpass.parky.service
 
 import io.devpass.parky.entity.ParkingSpot
+import io.devpass.parky.entity.Vehicle
 import io.devpass.parky.framework.getOrNull
 import io.devpass.parky.repository.ParkingSpotRepository
 import org.springframework.stereotype.Service
+@Suppress("UNCHECKED_CAST")
 @Service
 class ParkingSpotService(
     private val parkingSpotRepository: ParkingSpotRepository
 ) {
     fun findById(id: Int): ParkingSpot? {
         return parkingSpotRepository.findById(id).getOrNull()
+    }
+
+    fun findAllParkingSpot(): List<ParkingSpot> {
+        return parkingSpotRepository.findAll() as List<ParkingSpot>
     }
 }
