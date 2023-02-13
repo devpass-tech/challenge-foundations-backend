41fba5cb1b4fc04dea1d92aa6c9087e390315f07
bino1 <r.asera@outlook.com>
2023-02-13 20:37:13 -0300

    Atualizando Controller, Adicionando mais dois parametros de filtro: floor e spot

--- solutions/devsprint-michel-ferreira-2/src/main/kotlin/io/devpass/parky/controller/ParkingSpotController.kt
+++ solutions/devsprint-michel-ferreira-2/src/main/kotlin/io/devpass/parky/controller/ParkingSpotController.kt
@@ -3,6 +3,7 @@ package io.devpass.parky.controller
 import io.devpass.parky.entity.ParkingSpot
 import io.devpass.parky.service.ParkingSpotService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -12,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController
 class ParkingSpotController (
     private val parkingSpotService: ParkingSpotService
 ){
-    @GetMapping("/v1/listParkingSpot")
+    @GetMapping("/listParkingSpot")
     fun listParkingSpot(
-        @RequestParam( name = "inUse", required = false) emUso: Boolean?): List<ParkingSpot>{
+        @PathVariable id: Long,
+        @RequestParam(required = false) floor: Int,
+        @RequestParam(required = false) spot: Int,
+        @RequestParam( name = "inUse", required = false) InUseBy: Boolean?): List<ParkingSpot>{
         return parkingSpotService.listParkingSpotService()
     }
 }
